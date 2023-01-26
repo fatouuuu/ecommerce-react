ff1df5b2145509e267e1a0b35ce4819cd15bd5ab
fatouuuu <fatmaabdullrahim@gmail.com>
2023-01-27 02:57:16 +0300

    fixed directory structure and added firebase for user authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,25 @@
-import firebase from '../package.json';
-import 'firebase/firestore';
-import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD1cH3dqpu7hhYFvS0PJiBeb3eUWySFtiI",
-    authDomain: "ecommerce-project-1361d.firebaseapp.com",
-    projectId: "ecommerce-project-1361d",
-    storageBucket: "ecommerce-project-1361d.appspot.com",
-    messagingSenderId: "412058843795",
-    appId: "1:412058843795:web:73735bad8064264831ab66",
-    measurementId: "G-ZLSH8VZDG0"
-  };
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD1cH3dqpu7hhYFvS0PJiBeb3eUWySFtiI",
+  authDomain: "ecommerce-project-1361d.firebaseapp.com",
+  projectId: "ecommerce-project-1361d",
+  storageBucket: "ecommerce-project-1361d.appspot.com",
+  messagingSenderId: "412058843795",
+  appId: "1:412058843795:web:73735bad8064264831ab66",
+  measurementId: "G-ZLSH8VZDG0"
+};
 
 
-const auth = firebaseApp.auth();
+const app = initializeApp(firebaseConfig);
 
-export { auth };
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, auth };
+
+
+
